58daf862602ea544067dfcf5ce84810418f54d61
kryota226 <34178092+kryota226@users.noreply.github.com>
2018-11-17 11:47:52 +0900

    Add files via upload

---
+++ FastAlexNet/include/layers/linear.hpp
@@ -0,0 +1,24 @@
+#pragma once
+#include "utils/tensor.hpp"
+
+
+template <typename T>
+class Linear
+{
+public:
+    typedef T InT;
+    typedef T OutT;
+
+    Linear(const double multiplier=1.0, const double accumulator=0.0)
+        : multiplier_(multiplier), accumulator_(accumulator)
+    {}
+
+    OutT forward(const InT & src) const
+    {
+        return src * multiplier_ + accumulator_;
+    }
+
+private:
+    double multiplier_;
+    double accumulator_;
+};
